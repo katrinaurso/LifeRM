4d6fef3211c54c89b786aba5c853c6962d73f468
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-18 14:17:47 -0800

    Saving current progress

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,5 +1,6 @@
 var dashboard = angular.module('dashboard', ['ngRoute']);
-// var contacts = [
-// 	{name: 'Katrina'},
-// 	{name: 'Tim'}
-// ];
+// dashboard.config(function($routeProvider){
+// 	$routeProvider
+// 	.when('/main', {templateUrl: 'partials/main.html'})
+// 	.when('/customers', {templateUrl: 'partials/customers.html'});
+// });
