3e3e1ca279b5f124901e0b5eb3f78a59d553819f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-18 08:05:22 -0800

    Fixed routing problem

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
-var dahsboard = require('./controllers/dashboard.js');
+var dashboard = require('./../controllers/dashboard.js');
 
 module.exports = function Routes(app){
 	app.get('/', function (req, res) {
