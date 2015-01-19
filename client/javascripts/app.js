30e836ef8159f09f6c77030942371a193ce28c8e
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-18 20:24:52 -0800

    Saving current progress

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,6 +1,21 @@
 var dashboard = angular.module('dashboard', ['ngRoute']);
 // dashboard.config(function($routeProvider){
 // 	$routeProvider
-// 	.when('/main', {templateUrl: 'partials/main.html'})
-// 	.when('/customers', {templateUrl: 'partials/customers.html'});
+// 	.when('/', {
+// 		templateUrl: 'partials/main.html',
+// 		controller: 'ContactController'
+// 	})
+// 	.when('/contact', {templateUrl: 'partials/main.html'});
+// });
+
+
+/// Test Section /////
+// var dashboard = angular.module('dashboard', ['ui-router']);
+// dashboard.config(function($stateProvider, $urlRouterProvider){
+// 	$urlRouterProvider.otherwise('/home');
+// 	$stateProvider
+// 		.state('home', {
+// 			url: '/',
+// 			templateUrl: 'partials/main.html'
+// 		});
 // });
